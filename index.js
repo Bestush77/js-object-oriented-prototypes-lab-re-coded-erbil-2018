8bf8150cf1d471e871a4f346ea4d4b25e47daee2
Bestun Gardi <bestun95@yahoo.com>
2018-09-04 07:20:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function BoardMember(name, homeState, training){
   this.name = name
   this.homeState = homeState
   this.training = training
-
-  this.veto = function(){
+}
+Boardmember.prototype.veto = function(){
     return 'No, I must disagree'
   }
   this.approve = function(){
